17c1eab52f332e01354f5fbdac63a9a727da0bf5
Fallensn0w <thimcederlund@hotmail.com>
2010-08-24 16:47:45 +0200

    first commit

---
+++ iCENTER/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(308, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Install Game Center.app for iOS 4.X" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(This will take 1-2 minutes)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Font = New System.Drawing.Font("Arial Black", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(333, 30)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NOTE: This program requires Wi-Fi + OpenSSH!"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(308, 59)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Uninstall Game Center.app (Removes, cannot be undone unless install again.)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label2.Font = New System.Drawing.Font("Impact", 18.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Aqua
+        Me.Label2.Location = New System.Drawing.Point(0, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(333, 37)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "By: Fallensn0w"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 180)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(153, 21)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "Enter IP for iDevice Here!"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 208)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "iCENETER - Game Center.app installer"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ iCENTER/Form1.vb
@@ -0,0 +1,133 @@
+﻿
+Imports System
+Imports System.Diagnostics
+Imports System.Xml
+Imports System.Xml.XPath
+Imports System.IO
+
+Public Class Form1
+
+    ' TODO:
+    ' Fix respring by using cmd
+    ' launchctl stop com.apple.SpringBoard"
+
+    Dim WinSCP As Process = New Process()
+    Dim UsrName As String = "root"
+    Dim PassWord As String = "alpine"
+
+    Public Function SaveTextToFile(ByVal strData As String, ByVal FullPath As String, Optional ByVal ErrInfo As String = "") As Boolean
+        Dim bAns As Boolean = False, objReader As StreamWriter
+        Try
+            objReader = New StreamWriter(FullPath)
+            objReader.Write(strData)
+            objReader.Close()
+            bAns = True
+        Catch Ex As Exception
+            ErrInfo = Ex.Message
+        End Try
+        Return bAns
+    End Function
+
+
+    Sub ShellWait(ByVal file As String, ByVal arg As String)
+        Dim procNlite As New Process
+        winstyle = 1
+        procNlite.StartInfo.FileName = file
+        procNlite.StartInfo.Arguments = " " & arg
+        procNlite.StartInfo.WindowStyle = winstyle
+        Application.DoEvents()
+        procNlite.Start()
+        Do Until procNlite.HasExited
+            Application.DoEvents()
+            For i = 0 To 5000000
+                Application.DoEvents()
+            Next
+        Loop
+        procNlite.WaitForExit()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button1.Enabled = False : Button2.Enabled = False : Timer1.Stop()
+        Button1.Text = "Preparing Game Center.app"
+        ShellWait("unzip.exe", " Game_Center.zip")
+
+        Button1.Text = "Connecting to " & TextBox1.Text & "..."
+        WinSCP.StandardInput.WriteLine("open " & TextBox1.Text)
+
+        Button1.Text = "Logging in with information.."
+        WinSCP.StandardInput.WriteLine(UsrName)
+        WinSCP.StandardInput.WriteLine(PassWord)
+
+        Button1.Text = "Intializing for Game Center.app Installation"
+        WinSCP.StandardInput.WriteLine("cd /private/var/stash/Applications")
+
+        Button1.Text = "Uploading Game Center.app"
+        WinSCP.StandardInput.WriteLine("put ""Game Center.app""")
+
+        Button1.Text = "Appyling permission(s) to Game Center.app"
+        WinSCP.StandardInput.WriteLine("cd ""Game Center.app""")
+        WinSCP.StandardInput.WriteLine("chmod 0777 *")
+
+        Button1.Text = "Game Center.app was installed!" & vbCrLf & "Respring your iDevice!"
+        WinSCP.StandardInput.Close()
+
+        Dim a As String = WinSCP.StandardOutput.ReadToEnd()
+        SaveTextToFile(a, "output.txt")
+
+
+    End Sub
+
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        End
+        WinSCP.WaitForExit()         ' Wait until WinSCP finishes
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        WinSCP.StartInfo.FileName = "winscp.com"
+        WinSCP.StartInfo.UseShellExecute = False
+        WinSCP.StartInfo.RedirectStandardInput = True
+        WinSCP.StartInfo.RedirectStandardOutput = True
+        WinSCP.StartInfo.CreateNoWindow = True
+        WinSCP.Start()         ' Run hidden WinSCP process
+
+        WinSCP.StandardInput.WriteLine("option batch abort")
+        WinSCP.StandardInput.WriteLine("option confirm off")
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Button1.Enabled = False : Button2.Enabled = False : Timer1.Stop()
+        Button2.Text = "Connecting to " & TextBox1.Text & "..."
+        WinSCP.StandardInput.WriteLine("open " & TextBox1.Text)
+        Button2.Text = "Logging in with information.."
+        WinSCP.StandardInput.WriteLine(UsrName)
+        WinSCP.StandardInput.WriteLine(PassWord)
+        Button2.Text = "Deleting Game Center.app"
+        WinSCP.StandardInput.WriteLine("rmdir ""/private/var/stash/Applications/Game Center.app""")
+        '   rm -f "/private/var/stash/Applications/Game Center.app"
+        Button2.Text = "Game Center.app was deleted!" & vbCrLf & "Respring your iDevice!"
+        WinSCP.StandardInput.Close()
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If (TextBox1.Text = "Enter IP for iDevice Here!") Then
+            Button1.Enabled = False
+            Button2.Enabled = False
+        Else
+            Button1.Enabled = True
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        Process.Start("http://twitter.com/Fallensn0w")
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        MsgBox("This is because iCENTER will connect through SSH and install Game Center application to your iDevice. " & vbCrLf & vbCrLf & "Please, use Cydia and install these before trying this. Otherwise you will not succeed the installation!", MsgBoxStyle.OkOnly, Me.Text)
+    End Sub
+
+End Class
+
